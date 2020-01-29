8a4ca885ba7c322d20ced8624e42564fd8420734
AravindRavi95 <aravindravi2k14@gmail.com>
2020-01-29 18:17:32 +0530

    2 files modified

--- loginPage.js
+++ loginPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import firebase from './fire';
+import FileUploader from "react-firebase-file-uploader";
 
     const storage = firebase.storage();
     const files = [ 'image1.png', 'image2.png' ]; 
@@ -15,31 +16,60 @@ import firebase from './fire';
 export default class LoginPage extends Component {
 
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      email   : '',
-      password: '',
-    }
-    this.firestore = firebase.firestore(); // This works fine.
-    this.storage = firebase.storage(); // This fails
-    console.log( "Got storage ref" );
-  }
+  state = {
+    username: "",
+    avatar: "",
+    isUploading: false,
+    progress: 0,
+    avatarURL: ""
+  };
+ 
+  handleChangeUsername = event =>
+    this.setState({ username: event.target.value });
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+  handleProgress = progress => this.setState({ progress });
+  handleUploadError = error => {
+    this.setState({ isUploading: false });
+    console.error("error",error);
+  };
+  handleUploadSuccess = filename => {
+    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    firebase
+      .storage()
+      .ref("")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ avatarURL: url }));
+  };
   
 
   render() {
-    const storage = firebase.storage();
-    const files = [ 'image1.png', 'image2.png' ]; 
-    files.map( filename => {
-        storage
-          .ref( `/covers/${filename}` )
-          .getDownloadURL()
-          .then( url => {
-            console.log( "Got download url: ", url );
-          });
-    });
+    console.log(this.file)
     return (
-      <input type="file" ref={this.setRef}/>
+      <div>
+        <form>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={this.state.username}
+            name="username"
+            onChange={this.handleChangeUsername}
+          />
+          <label>Avatar:</label>
+          {this.state.isUploading && <p>Progress: {this.state.progress}</p>}
+          {this.state.avatarURL && <img src={this.state.avatarURL} />}
+          <FileUploader
+            accept="image/*"
+            name="avatar"
+            randomizeFilename
+            storageRef={firebase.storage().ref("")}
+            onUploadStart={this.handleUploadStart}
+            onUploadError={this.handleUploadError}
+            onUploadSuccess={this.handleUploadSuccess}
+            onProgress={this.handleProgress}
+          />
+        </form>
+      </div>
     );
   }
 }
